3cc091496d317410d27ce9b63a69d323a0f7aa5f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-11-15 16:55:08 -0500

    conectando app service

--- models/DataBase.php
+++ models/DataBase.php
@@ -26,7 +26,5 @@
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $pdo;
         }
-        
-        ## https://php-limpio-fpeccygaf2czhjbg.canadacentral-01.azurewebsites.net/
 	}
 ?>
